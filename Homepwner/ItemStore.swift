ddab69fc3137907e59cce46fbe782c1091e7d2bf
Ewerton Madruga <emadruga@gmail.com>
2016-04-12 11:53:41 -0300

    Added mods that make the version final

--- Homepwner/ItemStore.swift
+++ Homepwner/ItemStore.swift
@@ -6,4 +6,22 @@
 //  Copyright © 2016 Ewerton Madruga. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ItemStore {
+    var allItems = [Item]()
+    
+    init() {
+        for _ in 0..<5 {
+            createItem()
+        }
+    }
+    
+    func createItem() -> Item {
+        let newItem = Item(random: true)
+        allItems.append(newItem)
+    
+        return newItem
+    }
+}
+
