504158ec89ac3fd74421c12d8c1cad53649dd5e2
Ewerton Madruga <emadruga@gmail.com>
2016-04-25 15:59:44 -0300

    added Edit and Add buttons.

--- Homepwner/ItemStore.swift
+++ Homepwner/ItemStore.swift
@@ -10,18 +10,33 @@ import UIKit
 
 class ItemStore {
     var allItems = [Item]()
-    
-    init() {
-        for _ in 0..<5 {
-            createItem()
-        }
-    }
-    
+        
     func createItem() -> Item {
         let newItem = Item(random: true)
         allItems.append(newItem)
     
         return newItem
+    }
+    
+    func removeItem(item: Item) {
+        if let index = allItems.indexOf(item) {
+            allItems.removeAtIndex(index)
+        }
+    }
+    
+    func moveItemAtIndex(fromIndex: Int, toIndex: Int) {
+        if fromIndex == toIndex {
+            return
+        }
+        
+        // get reference to object being moved so you can reinsert it
+        let movedItem = allItems[fromIndex]
+        
+        // remove item from array
+        allItems.removeAtIndex(fromIndex)
+        
+        // insert item in array at new location
+        allItems.insert(movedItem, atIndex: toIndex)
     }
 }
 
--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -12,6 +12,36 @@ class ItemsViewController : UITableViewController {
     
     var itemStore: ItemStore!
     
+    @IBAction func addNewItem(sender: AnyObject) {
+        // create a new item and add it to the store
+        let newItem = itemStore.createItem()
+        
+        // figure out where this item is in the array
+        if let index = itemStore.allItems.indexOf(newItem) {
+            let indexPath = NSIndexPath(forRow: index, inSection: 0)
+            
+            // insert this new row into the table
+            tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+        }
+    }
+    
+    @IBAction func toggleEditingMode(sender: AnyObject) {
+        // if you are currently in editing mode...
+        if editing {
+            // change text of button to infor user state
+            sender.setTitle("Edit", forState: .Normal)
+            
+            // turn off editing mode
+            setEditing(false,animated: true)
+        } else {
+            // change text button to inform user state
+            sender.setTitle("Done", forState: .Normal)
+            
+            // enter editing mode
+            setEditing(true, animated: true)
+        }
+    }
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemStore.allItems.count
     }
@@ -28,6 +58,25 @@ class ItemsViewController : UITableViewController {
         cell.detailTextLabel?.text = "$\(item.valueInDollars)"
         
         return cell
+    }
+    
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        // if the table view is asking to commit a delete command...
+        if editingStyle == .Delete {
+            let item = itemStore.allItems[indexPath.row]
+            
+            // remove the item from the store
+            itemStore.removeItem(item)
+            
+            // also remove that row from the table view with an animation
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+        }
+    }
+    
+    override func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
+        // update the model
+        itemStore.moveItemAtIndex(sourceIndexPath.row, toIndex: destinationIndexPath.row)
+
     }
     
     override func viewDidLoad() {
