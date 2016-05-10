f48cb56a9e3858b49e49e33757cf18862997a1df
Helio <helio@dmtics-mac-mini.local>
2016-05-10 10:34:07 -0300

    added date picker view controller, and the proper wirings from the "change date" button in the detail view controller --- still need to save the picked date into the model.

---
+++ Homepwner/DatePickerViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DatePickerViewController.swift
+//  Homepwner
+//
+//  Created by Helio on 5/9/16.
+//  Copyright © 2016 Ewerton Madruga. All rights reserved.
+//
+
+import UIKit
+
+class DatePickerViewController: UIViewController {
+    
+    @IBOutlet var datePicker: UIDatePicker!
+}
