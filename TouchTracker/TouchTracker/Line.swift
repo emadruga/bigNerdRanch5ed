1b4936e10889861cd7467811cfc5f67926dc0a3f
Ewerton Madruga <emadruga@gmail.com>
2016-05-24 14:44:02 -0300

    added single line drawing

---
+++ TouchTracker/TouchTracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Ewerton Madruga on 5/23/16.
+//  Copyright © 2016 Ewerton Madruga. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
