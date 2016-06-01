5037d67f4990ff713a7f59c6aa91424c4743eb0d
Ewerton Madruga <emadruga@gmail.com>
2016-06-01 14:08:36 -0300

    clear screen with a double tap, and select a line with a single tap.

--- TouchTracker/TouchTracker/DrawView.swift
+++ TouchTracker/TouchTracker/DrawView.swift
@@ -11,6 +11,7 @@ import UIKit
 class DrawView: UIView {
     var currentLines = [NSValue:Line]()
     var finishedLines = [Line]()
+    var selectedLineIndex: Int?
     
     @IBInspectable var finishedLineColor: UIColor = UIColor.blackColor() {
         didSet {
@@ -51,6 +52,74 @@ class DrawView: UIView {
         for (_,line) in currentLines {
             strokeLine(line)
         }
+        
+        if let index = selectedLineIndex {
+            UIColor.greenColor().setStroke()
+            let selectedLine = finishedLines[index]
+            strokeLine(selectedLine)
+        }
+    }
+    
+    func indexOfLineAtPoint(point: CGPoint) -> Int? {
+        // find a line close to a point
+        for (index,line) in finishedLines.enumerate() {
+            let begin = line.begin
+            let end = line.end
+            
+            // check a few points on the line
+            for t in CGFloat(0).stride(to: 1.0, by: 0.05) {
+                let x = begin.x + ((end.x - begin.x) * t)
+                let y = begin.y + ((end.y - begin.y) * t)
+                
+                // if the tapped point is within 20 points, this the line to return
+                if hypot(x - point.x, y - point.y) < 20.0 {
+                    return index
+                }
+            }
+        }
+        // if nothing is close enough, we do not select a line
+        return nil
+    }
+    
+    //
+    // MARK: - constructors & tap handlers
+    //
+    
+    func doubleTap(gestureRecognizer: UIGestureRecognizer) {
+        print ("Recognized a double tap")
+        
+        selectedLineIndex = nil
+        currentLines.removeAll(keepCapacity: false)
+        finishedLines.removeAll(keepCapacity: false)
+        
+        setNeedsDisplay()
+    }
+    
+    func tap(gestureRecognizer: UIGestureRecognizer) {
+        print ("Recognized a single tap")
+        
+        let point = gestureRecognizer.locationInView(self)
+        selectedLineIndex = indexOfLineAtPoint(point)
+        
+        setNeedsDisplay()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        let doubleTapRecognizer = UITapGestureRecognizer(target: self,
+                                                         action: #selector(DrawView.doubleTap(_:)))
+        
+        doubleTapRecognizer.numberOfTapsRequired = 2
+        doubleTapRecognizer.delaysTouchesBegan = true
+        addGestureRecognizer(doubleTapRecognizer)
+
+        let tapRecognizer = UITapGestureRecognizer(target: self,
+                                                         action: #selector(DrawView.tap(_:)))
+        tapRecognizer.delaysTouchesBegan = true
+        tapRecognizer.requireGestureRecognizerToFail(doubleTapRecognizer)
+        addGestureRecognizer(tapRecognizer)
+
     }
     
     //
