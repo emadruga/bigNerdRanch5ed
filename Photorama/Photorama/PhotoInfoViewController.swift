e918b9bec864b3da532779f8a207d87a4c8c65b6
Ewerton Madruga <emadruga@gmail.com>
2016-06-24 16:20:45 -0300

    added a new image view controller where a selected photo from collection view shall be displayed alone.

---
+++ Photorama/Photorama/PhotoInfoViewController.swift
@@ -0,0 +1,40 @@
+//
+//  PhotoInfoViewController.swift
+//  Photorama
+//
+//  Created by Ewerton Madruga on 6/24/16.
+//  Copyright © 2016 Ewerton Madruga. All rights reserved.
+//
+
+import UIKit
+
+class PhotoInfoViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    var photo: Photo! {
+        didSet {
+            navigationItem.title = photo.title
+        }
+    }
+    
+    var store: PhotoStore!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        store.fetchImageForPhoto(photo) { (result) -> Void in
+            
+            switch result {
+            case let .Success(image):
+                NSOperationQueue.mainQueue().addOperationWithBlock() {
+                    self.imageView.image = image
+                }
+            case let .Failure(error):
+                print("Error fetching image for photo: \(error)")
+            }
+            
+        }
+    }
+    
+}
