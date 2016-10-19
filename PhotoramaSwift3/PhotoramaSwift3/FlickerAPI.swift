3e36cd43d0a28524b9343e9b18b5a1679a4eb6a7
Ewerton Madruga <emadruga@gmail.com>
2016-10-19 10:51:14 -0200

    preparing for photo display...

--- PhotoramaSwift3/PhotoramaSwift3/FlickerAPI.swift
+++ PhotoramaSwift3/PhotoramaSwift3/FlickerAPI.swift
@@ -12,10 +12,38 @@ enum Method: String {
     case RecentPhotos = "flickr.photos.getRecent"
 }
 
+enum PhotosResult {
+    case Success([Photo])
+    case Failure(Error)
+}
+
+enum FlickrError: Error {
+    case InvalidJSONData
+}
+
 struct FlickrAPI {
     private static let baseURLString = "https://api.flickr.com/services/rest"
     private static let APIKey = "a6d819499131071f158fd740860a5a88"
+    private static let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        return formatter
+    }()
 
+    private static func photoFromJSONObject(json: [String : Any]) -> Photo? {
+        guard let
+            photoID = json["id"]  as? String,
+            let title = json["title"] as? String,
+            let dateString = json["datetaken"] as? String,
+            let photoURLString = json["url_h"] as? String,
+            let url = URL(string: photoURLString),
+            let dateTaken = dateFormatter.date(from: dateString) else {
+                // do not hav enough info to construct a photo
+                return nil
+        }
+        return Photo(title: title, photoID: photoID, remoteURL: url, dateTaken: dateTaken)
+    }
+    
     private static func flickrURL(method: Method, parameters: [String:String]?) -> URL {
         
         var components = URLComponents(string: baseURLString)!
@@ -49,5 +77,41 @@ struct FlickrAPI {
     static func recentPhotosURL() -> URL {
         return flickrURL(method: .RecentPhotos, parameters: ["extras": "url_h,date_taken"])
     }
+    
+    static func photosFromJSONData(data: Data) -> PhotosResult {
+        do {
+            let jsonObject: Any
+                = try JSONSerialization.jsonObject(with: data, options: [])
+            
+            guard let
+                jsonDictionary = jsonObject as? [String:Any],
+                let photos = jsonDictionary["photos"] as? [String:Any],
+                let photosArray = photos["photo"] as? [[String:Any]] else {
+                    // the JSON structure does not match our expectations
+                    return .Failure(FlickrError.InvalidJSONData)
+            }
+
+            var finalPhotos = [Photo]()
+            
+            for photoJSON in photosArray {
+                if let photo = photoFromJSONObject(json: photoJSON) {
+                    finalPhotos.append(photo)
+                }
+            }
+            
+            if finalPhotos.count == 0 && photosArray.count > 0 {
+                // we weren't able to parse any of the photos
+                // maybe the JSON format for photos has changed
+                return .Failure(FlickrError.InvalidJSONData)
+            }
+            
+            return .Success(finalPhotos)
+            
+        }
+        catch let error {
+            return .Failure(error)
+        }
+    }
+
 
 }
--- PhotoramaSwift3/PhotoramaSwift3/PhotoStore.swift
+++ PhotoramaSwift3/PhotoramaSwift3/PhotoStore.swift
@@ -14,31 +14,24 @@ class PhotoStore {
         return URLSession(configuration: config)
     }()
     
-    func fetchRecentPhotos() {
+    func fetchRecentPhotos(completion: @escaping (PhotosResult) -> Void) {
         
         let url = FlickrAPI.recentPhotosURL()
         let request = URLRequest(url: url)
         let task = session.dataTask(with: request) {
             (data,response,error) -> Void in
             
-            if let jsonData = data {
-                do {
-                    let jsonObject: Any
-                        = try JSONSerialization.jsonObject(with: jsonData, options: [])
-                    
-                    print(jsonObject)
-                }
-                catch let error {
-                    print("Error creating JSON object: \(error)")
-                }
-            }
-            else if let requestError = error {
-                print("Error fetching recent photos: \(requestError)")
-            }
-            else {
-                print("Unexpected error with the request")
-            }            
+            let result = self.processRecentPhotosRequest(data: data, error: error)
+            completion(result)
         }
         task.resume()
     }
+    
+    func processRecentPhotosRequest(data: Data?, error: Error?) -> PhotosResult {
+        guard let jsonData = data else {
+            return .Failure(error!)
+        }
+        return FlickrAPI.photosFromJSONData(data: jsonData)
+    }
+
 }
